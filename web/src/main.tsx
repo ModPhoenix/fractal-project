652d2485d332207a5bfe224c74700cdef2482267
modphoenix <ivanchenko.b@gmail.com>
2024-09-08 15:36:42 +0300

    feat: use theme provider

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -2,12 +2,15 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { App } from "./App.tsx";
 import "./index.css";
-import { TooltipProvider } from "@/components/ui/tooltip.tsx";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { ThemeProvider } from "./components/theme-provider";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <TooltipProvider>
-      <App />
-    </TooltipProvider>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <TooltipProvider>
+        <App />
+      </TooltipProvider>
+    </ThemeProvider>
   </StrictMode>,
 );

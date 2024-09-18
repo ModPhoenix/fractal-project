5b1891e6875bf5338d1d3947898e9b27d7081b1b
modphoenix <ivanchenko.b@gmail.com>
2024-09-18 22:25:18 +0300

    feat: add tanstack/react-router

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,17 +1,30 @@
 import { createRoot } from "react-dom/client";
-import { App } from "./App.tsx";
 import "./index.css";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { ThemeProvider } from "./components/theme-provider";
 import { ApolloProvider } from "@apollo/client";
 import { client } from "@/api";
 import { Toaster } from "@/components/ui/toaster";
+import { RouterProvider, createRouter } from "@tanstack/react-router";
+
+// Import the generated route tree
+import { routeTree } from "./routeTree.gen";
+
+// Create a new router instance
+const router = createRouter({ routeTree });
+
+// Register the router instance for type safety
+declare module "@tanstack/react-router" {
+  interface Register {
+    router: typeof router;
+  }
+}
 
 createRoot(document.getElementById("root")!).render(
   <ApolloProvider client={client}>
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <TooltipProvider>
-        <App />
+        <RouterProvider router={router} />
         <Toaster />
       </TooltipProvider>
     </ThemeProvider>

9e84edd177264a80e75fcae64717d69b02a4e89a
modphoenix <ivanchenko.b@gmail.com>
2024-10-24 21:46:51 +0300

    chore: cleanup

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -6,13 +6,10 @@ import { ApolloProvider } from "@apollo/client";
 import { client } from "@/api";
 import { Toaster } from "@/components/ui/toaster";
 import { RouterProvider, createRouter } from "@tanstack/react-router";
-
-// Import the generated route tree
 import { routeTree } from "./routeTree.gen";
 
 // Create a new router instance
 const router = createRouter({ routeTree });
-
 // Register the router instance for type safety
 declare module "@tanstack/react-router" {
   interface Register {
